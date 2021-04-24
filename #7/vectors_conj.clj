3ba3d5b0869abf7c54c04d5177ce3709c16fd180
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 03:31:15 -0300

    Exercise 7

---
+++ #7/vectors_conj.clj
@@ -0,0 +1,14 @@
+; Vectors: conj
+
+; Difficulty:	Elementary
+; Topics:
+
+; When operating on a Vector, the conj function will return a new vector with one or more items "added" to the end.
+
+; (= __ (conj [1 2 3] 4))
+
+; (= __ (conj [1 2] 3 4))
+
+; Solution
+
+(= [1 2 3 4] (conj [1 2] 3 4))
