c36d3a8cbb62cea1df93a53513a04043605664df
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 03:18:30 -0300

    Exercise 6

---
+++ #6/intro_to_vectors.clj
@@ -0,0 +1,14 @@
+; Intro to Vectors
+
+; Difficulty:	Elementary
+; Topics:	
+
+; Vectors can be constructed several ways. You can compare them with lists.
+
+; Note: the brackets [] surrounding the blanks __ are part of the test case.
+
+; (= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+; Solution
+
+(= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
