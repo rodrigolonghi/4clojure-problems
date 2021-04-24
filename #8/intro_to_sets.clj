782ebcc44f454a8f9e08bf883b70f331c7eb2d4a
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 03:58:25 -0300

    Exercise 8

---
+++ #8/intro_to_sets.clj
@@ -0,0 +1,14 @@
+; Intro to Sets
+
+; Difficulty:	Elementary
+; Topics:	
+
+; Sets are collections of unique values.
+
+; (= __ (set '(:a :a :b :c :c :c :c :d :d)))
+
+; (= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+; Solution
+
+#{:a :b :c :d}
