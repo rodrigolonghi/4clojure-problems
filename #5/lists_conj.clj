6885aff33afc21a136c3578a227b8cf4a313f6b2
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 03:04:56 -0300

    Exercise 5

---
+++ #5/lists_conj.clj
@@ -0,0 +1,16 @@
+; Lists: conj
+
+; Difficulty:	Elementary
+; Topics:
+
+; When operating on a list, the conj function will return a new list with one or more items "added" to the front.
+
+; Note that there are two test cases, but you are expected to supply only one answer, which will cause all the tests to pass.
+
+; (= __ (conj '(2 3 4) 1))
+
+; (= __ (conj '(3 4) 2 1))
+
+; Solution
+
+(= '(1 2 3 4) (conj '(2 3 4) 1))
