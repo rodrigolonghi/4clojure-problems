0e36c6facda065a54c9c56fb2ce325bf55a022f9
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 02:38:32 -0300

    Exercise 1

--- #1/nothing_but_the_truth.clj
+++ #1/nothing_but_the_truth.clj
@@ -1,7 +1,7 @@
 ; Nothing but the Truth
  
 ; Difficulty:	Elementary
-; Topics:	
+; Topics:
 
 ; This is a clojure form. Enter a value which will make the form evaluate to true. Don't over think it! If you are confused, see the getting started page. Hint: true is equal to true.
 
