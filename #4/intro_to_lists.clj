eba29b38bc8d93a1933dac7daa6cec1f52faef10
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-24 02:27:19 -0300

    Exercise 4

---
+++ #4/intro_to_lists.clj
@@ -0,0 +1,12 @@
+; Intro to Lists
+
+; Difficulty:	Elementary
+; Topics:
+
+; Lists can be constructed with either a function or a quoted form.
+
+; (= (list __) '(:a :b :c))
+
+; Solution
+
+(= (list :a :b :c) '(:a :b :c))
