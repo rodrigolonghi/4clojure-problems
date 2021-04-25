93e25b35fba4179e077a5c029f676f7e4bb0d5c8
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 03:48:11 -0300

    Exercise 17

---
+++ #017/sequences_map.clj
@@ -0,0 +1,12 @@
+;; Sequences: map
+
+;; Difficulty:	Elementary
+;; Topics:
+
+;; The map function takes two arguments: a function (f) and a sequence (s). Map returns a new sequence consisting of the result of applying f to each item of s. Do not confuse the map function with the map data structure.
+
+;; (= __ (map #(+ % 5) '(1 2 3)))
+
+;; Solution
+
+'(6 7 8)
