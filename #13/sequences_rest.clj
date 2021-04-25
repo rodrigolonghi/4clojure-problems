9c0a4a77ead7d302f83867abde278176973b7aef
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 02:25:12 -0300

    Exercise 13

---
+++ #13/sequences_rest.clj
@@ -0,0 +1,12 @@
+;; Sequences: rest
+ 
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; The rest function will return all the items of a sequence except the first.
+
+;; (= __ (rest [10 20 30 40]))
+
+;; Solution
+
+[20 30 40]
