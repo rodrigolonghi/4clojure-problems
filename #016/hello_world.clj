6dfd85a3bf4ad95a9d8214e7c1b849326361230c
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 03:38:12 -0300

    Exercise 16

---
+++ #016/hello_world.clj
@@ -0,0 +1,16 @@
+;; Hello World
+ 
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; Write a function which returns a personalized greeting.
+
+;; (= (__ "Dave") "Hello, Dave!")
+
+;; (= (__ "Jenn") "Hello, Jenn!")
+
+;; (= (__ "Rhea") "Hello, Rhea!")
+
+;; Solution
+
+#(str "Hello, " % "!")
