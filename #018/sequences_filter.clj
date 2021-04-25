67c3f4a8a4168d4085ad2e0a733605964649a484
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 03:50:39 -0300

    Exercise 18

---
+++ #018/sequences_filter.clj
@@ -0,0 +1,12 @@
+;; Sequences: filter
+
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; The filter function takes two arguments: a predicate function (f) and a sequence (s). Filter returns a new sequence consisting of all the items of s for which (f item) returns true.
+
+;; (= __ (filter #(> % 5) '(3 4 5 6 7)))
+
+;; Solution
+
+'(6 7)
