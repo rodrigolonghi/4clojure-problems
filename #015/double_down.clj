74c5bccc12b3b50df83c08209f4e275c3d2c97de
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 03:11:32 -0300

    Exercise 15

---
+++ #015/double_down.clj
@@ -0,0 +1,18 @@
+;; Double Down
+
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; Write a function which doubles a number.
+
+;; (= (__ 2) 4)
+
+;; (= (__ 3) 6)
+
+;; (= (__ 11) 22)
+
+;; (= (__ 7) 14)
+
+;; Solution
+
+(fn [x] (* x 2))
