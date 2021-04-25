b0ecc8b975cc623606176cc09d8e9350a95c8e6e
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 01:59:29 -0300

    Exercise 11

---
+++ #11/maps_conj.clj
@@ -0,0 +1,13 @@
+;; Maps: conj
+ 
+;; Difficulty:	Elementary
+;; Topics:	
+
+
+;; When operating on a map, the conj function returns a new map with one or more key-value pairs "added".
+
+;; (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3]))
+
+;; Solution
+
+{:b 2}
