4754174a0e1333ef9bf9b71b54bbf1eb58396ca8
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 01:54:46 -0300

    Exercise 10

---
+++ #10/intro_to_maps.clj
@@ -0,0 +1,14 @@
+;; Intro to Maps
+ 
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; Maps store key-value pairs. Both maps and keywords can be used as lookup functions. Commas can be used to make maps more readable, but they are not required.
+
+;; (= __ ((hash-map :a 10, :b 20, :c 30) :b))
+
+;; (= __ (:b {:a 10, :b 20, :c 30}))
+
+;; Solution:
+
+20
