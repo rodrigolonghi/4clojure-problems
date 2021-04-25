1e33738895ff71777133adfd21791a57fb9f355b
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 03:04:12 -0300

    Exercise 14

---
+++ #014/intro_to_functions.clj
@@ -0,0 +1,18 @@
+;; Intro to Functions
+ 
+;; Difficulty:	Elementary
+;; Topics:	
+
+;; Clojure has many different ways to create functions.
+
+;; (= __ ((fn add-five [x] (+ x 5)) 3))
+
+;; (= __ ((fn [x] (+ x 5)) 3))
+
+;; (= __ (#(+ % 5) 3))
+
+;; (= __ ((partial + 5) 3))
+
+;; Solution
+
+8
