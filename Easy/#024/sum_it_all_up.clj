749a89006f77d191f0de0f16847c5b9912a6e75e
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-26 04:56:27 -0300

    Exercise 24

---
+++ Easy/#024/sum_it_all_up.clj
@@ -0,0 +1,21 @@
+;; Sum It All Up
+ 
+;; Difficulty:	Easy
+;; Topics:	seqs
+
+
+;; Write a function which returns the sum of a sequence of numbers.
+
+;; (= (__ [1 2 3]) 6)
+
+;; (= (__ (list 0 -2 5 5)) 8)
+
+;; (= (__ #{4 2 1}) 7)
+
+;; (= (__ '(0 0 -1)) -1)
+
+;; (= (__ '(1 10 3)) 14)
+
+;; Solution
+
+reduce +
