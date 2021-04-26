f0aaa033c131d0c1cfbea466baba268273fc4df8
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-26 02:58:46 -0300

    Exercise 52

---
+++ Elementary/#052/intro_to_destructuring.clj
@@ -0,0 +1,12 @@
+;; Intro to Destructuring
+ 
+;; Difficulty:	Elementary
+;; Topics:	destructuring
+
+;; Let bindings and function parameter lists support destructuring.
+
+;; (= [2 4] (let [[a b c d e] [0 1 2 3 4]] __))
+
+;; Solution
+
+[c e]
