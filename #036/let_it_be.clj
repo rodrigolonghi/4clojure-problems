66d1efc4f2da251648f8bfe85b389957512b9757
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 21:24:54 -0300

    Exercise 36

---
+++ #036/let_it_be.clj
@@ -0,0 +1,16 @@
+;; Let it Be
+ 
+;; Difficulty:	Elementary
+;; Topics:	math syntax
+
+;; Can you bind x, y, and z so that these are all true?
+
+;; (= 10 (let __ (+ x y)))
+
+;; (= 4 (let __ (+ y z)))
+
+;; (= 1 (let __ z))
+
+;; Solution
+
+[x 7 y 3 z 1]
