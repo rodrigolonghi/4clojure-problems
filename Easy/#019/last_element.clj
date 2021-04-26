d1218026d0483cfd2c425d4f33a31b8fcfb074be
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-26 03:29:53 -0300

    Exercise 19

---
+++ Easy/#019/last_element.clj
@@ -0,0 +1,17 @@
+;; Last Element
+ 
+;; Difficulty:	Easy
+;; Topics:	seqs core-functions
+
+
+;; Write a function which returns the last element in a sequence.
+
+;; (= (__ [1 2 3 4 5]) 5)
+
+;; (= (__ '(5 4 3)) 3)
+
+;; (= (__ ["b" "c" "d"]) "d")
+
+;; Solution
+
+#(-> % (reverse) (first))
