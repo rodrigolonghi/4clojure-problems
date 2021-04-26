4336db5231f14a778ba286b1f0fd3691c9ca31db
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-26 04:52:41 -0300

    Exercise 23

---
+++ Easy/#023/reverse_a_sequence.clj
@@ -0,0 +1,17 @@
+;; Reverse a Sequence
+
+;; Difficulty:	Easy
+;; Topics:	seqs core-functions
+
+
+;; Write a function which reverses a sequence.
+
+;; (= (__ [1 2 3 4 5]) [5 4 3 2 1])
+
+;; (= (__ (sorted-set 5 7 2 7)) '(7 5 2))
+
+;; (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
+
+;; Solution
+
+#(into () %)
