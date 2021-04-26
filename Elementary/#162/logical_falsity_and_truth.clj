5048ae007fa9e7196caf0eff4d319e52c4c90187
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-26 02:32:52 -0300

    Exercise 162

--- Elementary/#162/logical_falsity_and_truth.clj
+++ Elementary/#162/logical_falsity_and_truth.clj
@@ -1,24 +1,24 @@
-Logical falsity and truth
+;; Logical falsity and truth
  
-Difficulty:	Elementary
-Topics:	logic
+;; Difficulty:	Elementary
+;; Topics:	logic
 
-In Clojure, only nil and false represent the values of logical falsity in conditional tests - anything else is logical truth.
+;; In Clojure, only nil and false represent the values of logical falsity in conditional tests - anything else is logical truth.
 
-(= __ (if-not false 1 0))
+;; (= __ (if-not false 1 0))
 
-(= __ (if-not nil 1 0))
+;; (= __ (if-not nil 1 0))
 
-(= __ (if true 1 0))
+;; (= __ (if true 1 0))
 
-(= __ (if [] 1 0))
+;; (= __ (if [] 1 0))
 
-(= __ (if [0] 1 0))
+;; (= __ (if [0] 1 0))
 
-(= __ (if 0 1 0))
+;; (= __ (if 0 1 0))
 
-(= __ (if 1 1 0))
+;; (= __ (if 1 1 0))
 
-Solution
+;; Solution
 
 1
