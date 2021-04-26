43b0742ccab2c68f9f58f3d31a4c294406ab6cf8
Rodrigo Longhi <rfslonghi@gmail.com>
2021-04-25 23:16:03 -0300

    Exercise 37

---
+++ #037/regular_expressions.clj
@@ -0,0 +1,12 @@
+;; Regular Expressions
+ 
+;; Difficulty:	Elementary
+;; Topics:	regex syntax
+
+;; Regex patterns are supported with a special reader macro.
+
+;; (= __ (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
+;; Solution
+
+"ABC"
